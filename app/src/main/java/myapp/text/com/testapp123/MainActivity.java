f92b24dc62f4e6f7b92284d7f687aec3858f78ea
admin <atish.kumar@redlemondigital.com>
2018-11-01 17:19:15 +0530

    Atish

--- app/src/main/java/myapp/text/com/testapp123/MainActivity.java
+++ app/src/main/java/myapp/text/com/testapp123/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /// Check Subversion
         // Test Commit
+        // Test commit from TestAppHithub
     }
 }
